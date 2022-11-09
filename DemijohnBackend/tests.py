e207b0f3dfab79a39651d747d28a93e0d1b61234
Mehmed Esad Akçam <esad@192.168.1.101>
2022-11-09 17:41:02 +0300

    login endpoint is implemented

--- DemijohnBackend/tests.py
+++ DemijohnBackend/tests.py
@@ -9,3 +9,12 @@ class SignupTestCase(APITestCase):
         response = self.client.post("/signup/", data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.get().username, "test")
+
+
+class Login(APITestCase):
+    def test_login(self):
+        data = {"username": "test", "password": "test"}
+        self.client.post("/signup/", data, format="json")
+        response = self.client.post("/login/", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.get().username, "test")
--- DemijohnBackend/views.py
+++ DemijohnBackend/views.py
@@ -8,11 +8,11 @@ import hashlib
 @api_view(["POST"])
 def signup(request):
     print(request.data)
-    username: str = request.data["username"]
+    username: str = request.data.get("username")
     user = User.objects.filter(username=username).count()
     if user > 0:
         return JsonResponse({"message": "Username already exists."}, status=400)
-    hashed_password = hashlib.md5(request.data["password"].encode()).hexdigest()
+    hashed_password = hashlib.md5(request.data.get("password").encode()).hexdigest()
     User.objects.create(username=username, password=hashed_password)
     return JsonResponse({"user": username}, status=201)
 
@@ -23,7 +23,11 @@ def login(request):
     check request.data["username"] and hashlib.md5(request.data["password"].encode()).hexdigest() against
     database if true return HTTP 200 else return HTTP 400
     """
-    pass
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if check_user(username) and check_password(username, password):
+        return JsonResponse({"user": username}, status=200)
+    return JsonResponse({"message": "Invalid username or password."}, status=400)
 
 
 @api_view(["GET"])
@@ -33,7 +37,12 @@ def get_previous_orders(request):
     {orders:[{bottle:5, water_type:still}, {bottle:10, water_type:sparkling}]} else return HTTP 400
     else return HTTP 400
     """
-    pass
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if check_user(username) and check_password(username, password):
+        orders = User.objects.get(username=username).orders
+        return JsonResponse({"orders": orders}, status=200)
+    return JsonResponse({"message": "Invalid username or password."}, status=400)
 
 
 @api_view(["GET"])
@@ -98,4 +107,22 @@ def add_item_to_cart(request):
     to the user's cart and return http 200
     if user not exists return http 400
     """
-    pass
+
+
+def check_user(username) -> bool:
+    if username is None:
+        return False
+    user = User.objects.filter(username=username).count()
+    if user > 0:
+        return True
+    return False
+
+
+def check_password(username, password) -> bool:
+    if password is None or username is None:
+        return False
+    hashed_password = hashlib.md5(password.encode()).hexdigest()
+    user = User.objects.filter(username=username, password=hashed_password).count()
+    if user > 0:
+        return True
+    return False
