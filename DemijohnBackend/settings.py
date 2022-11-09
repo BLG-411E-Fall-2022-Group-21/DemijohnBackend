aaf0fbfecfff2853df8ac124edd0b124f315a145
Mehmed Esad Akçam <m.esadakcam@hotmail.com>
2022-11-09 21:45:28 +0300

    Allowed hosts are updated

--- DemijohnBackend/settings.py
+++ DemijohnBackend/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+ALLOWED_HOSTS = ['*']
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 NOSE_ARGS = ['--nocapture',
              '--nologcapture',]
