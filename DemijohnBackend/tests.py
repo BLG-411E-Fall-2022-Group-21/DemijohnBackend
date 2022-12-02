cc8703d87e380106620f150d976f44443d227acb
sezercakir <cakirta18@itu.edu.tr>
2022-12-03 01:09:14 +0300

    test added

--- DemijohnBackend/tests.py
+++ DemijohnBackend/tests.py
@@ -5,13 +5,27 @@ import hashlib
 
 data = {"username": "test", "password": "test"}
 
- 
+
 class SignupTestCase(APITestCase):
-    def test_signup(self):
+    def test_signup_valid(self):
         data = {"username": "test", "password": "test"}
         response = self.client.post("/signup/", data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.get().username, "test")
+
+    def test_signup_invalid(self):
+        data = {"username": "test", "password": ""}
+        response = self.client.post("/signup/", data, format="json")
+        self.assertEqual(response.status_code, 400)
+
+    def test_already_user(self):
+        data = {"username": "test", "password": "test"}
+        response = self.client.post("/signup/", data, format="json")
+        self.assertEqual(201, response.status_code)
+
+        data_2 = {"username": "test", "password": "test"}
+        response = self.client.post("/signup/", data_2)
+        self.assertEqual(400, response.status_code)
 
 
 class Login(APITestCase):
@@ -22,12 +36,17 @@ class Login(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(User.objects.get().username, "test")
 
-class GetPreviousOrders(APITestCase):
-    pass
+    def test_login_invalid(self):
+        data = {"username": "test", "password": "test"}
+        self.client.post("/signup/", data, format="json")
+        data2 = {"username": "test2", "password": "test"}
+        response = self.client.post("/login/", data2, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertNotEqual(User.objects.get().username, "test2")
 
 
 
-#TODO: test get_previous_orders
+# TODO: test get_previous_orders
 # class GetPreviousOrders(APITestCase):
 #     def test_get_previous_orders(self):
 #         data = {"username": "test", "password": "test"}
@@ -35,7 +54,7 @@ class GetPreviousOrders(APITestCase):
 #         response = self.client.post("/login/", data, format="json")
 #         self.assertEqual(response.status_code, 200)
 #         user = User.objects.get(username="test")
-#         self.assertEqual(user.username, "test") 
+#         self.assertEqual(user.username, "test")
 #         # PreviousOrder.objects.create(user_id=user, bottle=5, water_type="still")
 #         hashed_password = hashlib.md5("test".encode()).hexdigest()
 #         aa =  User.objects.filter(username="test").count()
